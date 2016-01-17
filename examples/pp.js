005c44048e6a5ba92e5fd21abb505f84fb93b2ec
Aaron Bieber <aaron@bolddaemon.com>
2016-01-17 15:09:11 -0700

    example

---
+++ examples/pp.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// Quick example of a pledged node app.
+// usage:
+// $ echo '{"a":1,"b":3}' | ./pp.js
+
+var data = '', pledge = require('node-pledge');
+pledge.init('stdio ioctl rpath wpath');
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+	data += chunk;
+    }
+});
+
+process.stdin.on('end', function() {
+    data = JSON.parse(data);
+    console.log(JSON.stringify(data, null, 2));
+});
